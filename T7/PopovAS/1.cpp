f92337cbb54c9245b93bcd057795bb7a522b68d9
Popov Vladislav <flash.royal473@gmail.com>
2018-04-03 23:08:26 +0500

    Добавлена Лабораторная работа 7

---
+++ T7/PopovAS/1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	std::queue<int> q, q1;
+	int a;
+	for (int i = 0; i < 10; i++) {
+		std::cin >> a;
+		if (a % 2 == 0) {
+			q.push(a);
+		}
+		if (a % 2 == 1) {
+			q1.push(a);
+		}
+	}
+	std::cout << "Первый и последний члены четной очереди:" << q.front() << " " << q.back() << std::endl;
+	std::cout << "Первый и последний члены нечетной очереди:" << q1.front() << " " << q1.back() << std::endl;
+	std::cin >> a;
+	return 0;
+}
