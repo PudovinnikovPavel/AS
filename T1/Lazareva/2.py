746365dad9416710b83d0fb74ee155dab505a2b9
Vikrotia <37018426+Vikrotia@users.noreply.github.com>
2018-04-08 13:20:17 +0300

    Добавлено упражнение 1

--- T1/Lazareva/2.py
+++ T1/Lazareva/2.py
@@ -1,15 +1,15 @@
-N = int (input("Количество элементов в списке: "))
+N = int (input("╨Ъ╨╛╨╗╨╕╤З╨╡╤Б╤В╨▓╨╛ ╤Н╨╗╨╡╨╝╨╡╨╜╤В╨╛╨▓ ╨▓ ╤Б╨┐╨╕╤Б╨║╨╡:  "))
 list1 = [1] * N
 for i in range(N):
-	list1[i] = int(input("Элемент:"))
+	list1[i] = int(input("╨н╨╗╨╡╨╝╨╡╨╜╤В:"))
 print(list1)
-print("Максимальный элемент списка: ",max(list1))
-A = int (input("Количество элементов в списке: "))
+print("╨Ь╨░╨║╤Б╨╕╨╝╨░╨╗╤М╨╜╤Л╨╣ ╤Н╨╗╨╡╨╝╨╡╨╜╤В ╤Б╨┐╨╕╤Б╨║╨░: : ",max(list1))
+A = int (input("╨Ъ╨╛╨╗╨╕╤З╨╡╤Б╤В╨▓╨╛ ╤Н╨╗╨╡╨╝╨╡╨╜╤В╨╛╨▓ ╨▓ ╤Б╨┐╨╕╤Б╨║╨╡: "))
 list2 = [1] * A
 for i in range(A):
-	list2[i] = int(input("Элемент:"))
+	list2[i] = int(input("╨н╨╗╨╡╨╝╨╡╨╜╤В:"))
 print(list2)
-print("Максимальный элемент списка: ",max(list2))
+print("╨Ь╨░╨║╤Б╨╕╨╝╨░╨╗╤М╨╜╤Л╨╣ ╤Н╨╗╨╡╨╝╨╡╨╜╤В ╤Б╨┐╨╕╤Б╨║╨░:: ",max(list2))
 list1 = [i/max(list1) for i in list1]
 print(list1)
 list2 = [i/max(list2) for i in list2]
