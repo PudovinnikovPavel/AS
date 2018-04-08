97154940ba59f8fdde7fec3ce7e7dbc1042a4b81
Vikrotia <37018426+Vikrotia@users.noreply.github.com>
2018-04-08 13:13:50 +0300

    Добавлено упражнение 1

---
+++ T1/Lazareva/2.py
@@ -0,0 +1,16 @@
+N = int (input("Количество элементов в списке: "))
+list1 = [1] * N
+for i in range(N):
+	list1[i] = int(input("Элемент:"))
+print(list1)
+print("Максимальный элемент списка: ",max(list1))
+A = int (input("Количество элементов в списке: "))
+list2 = [1] * A
+for i in range(A):
+	list2[i] = int(input("Элемент:"))
+print(list2)
+print("Максимальный элемент списка: ",max(list2))
+list1 = [i/max(list1) for i in list1]
+print(list1)
+list2 = [i/max(list2) for i in list2]
+print(list2)
